38a871c4a732b8d6f04c44452e6b561bd3186d01
illisan <illiv.sandra@gmail.com>
2018-04-05 16:40:34 -0700

    testing commit

--- index.js
+++ index.js
@@ -89,19 +89,19 @@ controller.on('slash_command', function (slashCommand, message) {
 
             if (message.text === "") {
               // Master list of foodmoji
-              foodmoji = [":coffee:",":tea:",":sake:",":baby_bottle:", \
-                ":beer:",":beers:",":cocktail:",":tropical_drink:", \
-                ":wine_glass:",":fork_and_knife:",":pizza:",":hamburger:", \
-                ":fries:",":poultry_leg:",":meat_on_bone:",":spaghetti:", \
-                ":curry:",":fried_shrimp:",":bento:",":sushi:",":fish_cake:", \
-                ":rice_ball:",":rice_cracker:",":rice:",":ramen:",":stew:", \
-                ":oden:",":dango:",":egg:",":bread:",":doughnut:",":custard:", \
-                ":icecream:",":ice_cream:",":shaved_ice:",":birthday:", \
-                ":cake:",":cookie:",":chocolate_bar:",":candy:",":lollipop:", \
-                ":honey_pot:",":apple:",":green_apple:",":tangerine:", \
-                ":lemon:",":cherries:",":grapes:",":watermelon:", \
-                ":strawberry:",":peach:",":melon:",":banana:",":pear:", \
-                ":pineapple:",":sweet_potato:",":eggplant:",":tomato:", \
+              foodmoji = [":coffee:",":tea:",":sake:",":baby_bottle:", 
+                ":beer:",":beers:",":cocktail:",":tropical_drink:", 
+                ":wine_glass:",":fork_and_knife:",":pizza:",":hamburger:", 
+                ":fries:",":poultry_leg:",":meat_on_bone:",":spaghetti:", 
+                ":curry:",":fried_shrimp:",":bento:",":sushi:",":fish_cake:", 
+                ":rice_ball:",":rice_cracker:",":rice:",":ramen:",":stew:", 
+                ":oden:",":dango:",":egg:",":bread:",":doughnut:",":custard:", 
+                ":icecream:",":ice_cream:",":shaved_ice:",":birthday:", 
+                ":cake:",":cookie:",":chocolate_bar:",":candy:",":lollipop:", 
+                ":honey_pot:",":apple:",":green_apple:",":tangerine:", 
+                ":lemon:",":cherries:",":grapes:",":watermelon:", 
+                ":strawberry:",":peach:",":melon:",":banana:",":pear:", 
+                ":pineapple:",":sweet_potato:",":eggplant:",":tomato:", 
                 ":corn:"];
               // TODO Get three random foods
 
